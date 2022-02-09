45a105a1f637b67dd7534d7a625776f59a288fc9
deveshbhardwaj970 <80453377+deveshbhardwaj970@users.noreply.github.com>
2022-02-09 22:28:34 +0530

    Delete 21. Find The Sum Of Natural Number.py

--- 21. Find The Sum Of Natural Number.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Enter any number '))
-sum = 0
-if num < 0:
-    print('plz enter positive number')
-else:
-    for i in range(num+1):
-        sum = sum + i
-    print(f'The sum is {sum}')
-
