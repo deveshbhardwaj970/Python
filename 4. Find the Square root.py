52100f24a2b158fced95ff5ceca72eb8925a4607
deveshbhardwaj970 <80453377+deveshbhardwaj970@users.noreply.github.com>
2022-02-09 22:29:52 +0530

    Delete 4. Find the Square root.py

--- 4. Find the Square root.py
+++
@@ -1,9 +0,0 @@
-import cmath
-
-a = float(input('enter a number '))
-
-#             or
-sq_root = a ** .5
-
-print(f'square root of {a} is {cmath.sqrt(a)}')
-print(f'square root of {a} is {sq_root}')
